2ed130480ac4dada0fdd24a6912bfd9f9ecdf0a1
esme-02 <esmeralda11cruz@icloud.com>
2023-12-21 11:05:18 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var superheroes = require("superheroes")
+const supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySupervillainName);
